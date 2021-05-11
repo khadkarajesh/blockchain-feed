c80031daeb01fd77bba40b5c3c73f0b8e7291a70
rajesh <rajesh.k.khadka@gmail.com>
2021-05-11 11:37:20 +0200

    add methods to update and delete tweet

--- contracts/TwitterList.sol
+++ contracts/TwitterList.sol
@@ -2,18 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TwitterList {
-    mapping(uint256 => Tweet) tweets;
-    uint256 count= uint256(0);
+    mapping(int => Tweet) public tweets;
+    event TwitterCreated(int id, string txt);
+    event TwitterUpdated(int id, string txt);
+    event TwitterDeleted(int id);
+    int count= 0;
 
     struct Tweet {
         string txt;
-        uint256 id;
+        int id;
     }
 
-    function insertTweet(string memory txt) public returns(uint256){
+    function insertTweet(string memory txt) public{
         count++;
         Tweet  memory tweet = Tweet(txt, count);
         tweets[count] = Tweet(txt, count); 
-        return tweet.id;
+        emit TwitterCreated(tweet.id, tweet.txt);
+    }
+
+    function updateTweet(int  _id, string memory txt) public{
+        Tweet memory _tweet = tweets[_id];
+        _tweet.txt = txt;
+        tweets[_id] = _tweet;
+        emit TwitterUpdated(_tweet.id, _tweet.txt);
+    }
+
+    function deleteTweet(int _id) public {
+        delete tweets[_id];
+        emit TwitterDeleted(_id);
     }
 }
