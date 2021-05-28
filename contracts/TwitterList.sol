b0f8e426edc3270d1fb06f948c8d5a15c34a9561
Sudhamsu <sudhamsukhanal@gmail.com>
2021-05-28 20:19:15 +0200

    doc for TwitterList

--- contracts/TwitterList.sol
+++ contracts/TwitterList.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+/**
+ * @title TwitterList
+ * @dev Contract dedicated to the creation, update and deletion of new zombies
+ */
 contract TwitterList {
     mapping(int256 => Tweet) public tweets;
     event TwitterCreated(int256 id, string txt);
@@ -13,6 +17,10 @@ contract TwitterList {
         int256 id;
     }
 
+/*
+ * @title TwitterList
+ * @dev Contract dedicated to the insert the tweet
+ */
     function insertTweet(string memory txt) public {
         count++;
         Tweet memory tweet = Tweet(txt, count);
@@ -20,6 +28,10 @@ contract TwitterList {
         emit TwitterCreated(tweet.id, tweet.txt);
     }
 
+    /*
+ * @title TwitterList
+ * @dev Contract dedicated to the update the tweet
+ */
     function updateTweet(int256 _id, string memory txt) public {
         Tweet memory _tweet = tweets[_id];
         _tweet.txt = txt;
@@ -27,6 +39,10 @@ contract TwitterList {
         emit TwitterUpdated(_tweet.id, _tweet.txt);
     }
 
+    /*
+ * @title TwitterList
+ * @dev Contract dedicated to the delete the tweet
+ */
     function deleteTweet(int256 _id) public {
         delete tweets[_id];
         emit TwitterDeleted(_id);
