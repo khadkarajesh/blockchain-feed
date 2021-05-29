4c8cf252af8dd75e6d45e9fc1eb09657299cf574
rajesh <rajesh.k.khadka@gmail.com>
2021-05-29 15:49:47 +0200

    generate docs

--- contracts/TwitterList.sol
+++ contracts/TwitterList.sol
@@ -3,23 +3,54 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TwitterList {
     mapping(int256 => Tweet) public tweets;
+    /**
+     * @dev event to notify twitter post creation
+     * @param id tweet id
+     * @param txt tweet
+     */
     event TwitterCreated(int256 id, string txt);
+
+     /**
+     * @dev event to notify twitter update
+     * @param id tweet id
+     * @param txt tweet
+     */
     event TwitterUpdated(int256 id, string txt);
+
+    /**
+     * @dev event to notify twitter delete
+     * @param id tweet id
+     */
+
     event TwitterDeleted(int256 id);
     int256 public count = 0;
 
+    /**
+     * @dev structure to hold tween information
+     * @param txt tweet text
+     * @param id tweet id
+     */
     struct Tweet {
         string txt;
         int256 id;
     }
 
-    function insertTweet(string memory txt) public {
+    /**
+     * @dev A function to insert tweet
+     * @param _tweet content of the tweet
+     */
+    function insertTweet(string memory _tweet) public {
         count++;
-        Tweet memory tweet = Tweet(txt, count);
-        tweets[count] = Tweet(txt, count);
+        Tweet memory tweet = Tweet(_tweet, count);
+        tweets[count] = Tweet(_tweet, count);
         emit TwitterCreated(tweet.id, tweet.txt);
     }
 
+    /**
+     * @dev A function to update tweet
+     * @param _id identifier of tweet
+     * @param txt updated tweet content
+     */
     function updateTweet(int256 _id, string memory txt) public {
         Tweet memory _tweet = tweets[_id];
         _tweet.txt = txt;
@@ -27,6 +58,10 @@ contract TwitterList {
         emit TwitterUpdated(_tweet.id, _tweet.txt);
     }
 
+    /**
+     * @dev A function to delete tweet
+     * @param _id identifier of tweet
+     */
     function deleteTweet(int256 _id) public {
         delete tweets[_id];
         emit TwitterDeleted(_id);
---
+++ docs/TwitterList.md
@@ -0,0 +1,50 @@
+## `TwitterList`
+
+
+
+
+
+
+### `insertTweet(string _tweet)` (public)
+
+
+
+A function to insert tweet
+
+
+### `updateTweet(int256 _id, string txt)` (public)
+
+
+
+A function to update tweet
+
+
+### `deleteTweet(int256 _id)` (public)
+
+
+
+A function to delete tweet
+
+
+
+### `TwitterCreated(int256 id, string txt)`
+
+
+
+event to notify twitter post creation
+
+
+### `TwitterUpdated(int256 id, string txt)`
+
+
+
+event to notify twitter update
+
+
+### `TwitterDeleted(int256 id)`
+
+
+
+event to notify twitter delete
+
+
